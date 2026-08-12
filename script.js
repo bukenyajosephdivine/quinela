708d73fc5b23431e91a6871753f8bac67a82a7e8
bukenyajosephdivine@ <bukenyajosephdivine@gmail.com>
2026-08-12 20:37:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://wing-town-experimental-brake.trycloudflare.com";
+const API_URL = "https://upcoming-clearly-anyway-dns.trycloudflare.com";
 
 async function sendMessage() {
 
