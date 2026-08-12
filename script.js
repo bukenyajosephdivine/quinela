dfe7ddf110e75012405c5996a675804de78e0731
bukenyajosephdivine@ <bukenyajosephdivine@gmail.com>
2026-08-12 22:15:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://appears-defense-fifteen-carries.trycloudflare.com";
+const API_URL = "https://translate-multiple-game-ant.trycloudflare.com";
 
 async function sendMessage() {
 
